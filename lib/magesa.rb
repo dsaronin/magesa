02dfd23be21d208943a1ae02e6b52aa12aaa4342
Daudi Amani <dsaronin@gmail.com>
2011-11-23 00:20:32 -0800

    0.1.2 dynamic version detection

--- lib/magesa.rb
+++ lib/magesa.rb
@@ -1,3 +1,8 @@
 # set up an environment variable to tell app where to find wkhtmlpdf binary
-ENV['MAGESA_PATH'] = [ ENV['GEM_PATH'], 'magesa-0.1.0', 'bin', 'wkhtmltopdf-amd64' ].join("/")
-# TODO: make the version dynamic by reading VERSION
+version = File.exist?('VERSION') ? File.read('VERSION') : "0.1.2"
+
+gem_path = ( ENV['GEM_PATH'].blank?  ?  
+            ENV['PATH'].split(':').first.sub(/\/bin/,"/gems")   :  
+            ENV['GEM_PATH'] 
+      )
+ENV['MAGESA_PATH'] = [ gem_path, "magesa-#{version}", 'bin', 'wkhtmltopdf-amd64' ].join("/")
