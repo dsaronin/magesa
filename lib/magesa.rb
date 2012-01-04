a9b00f59db01522834dea7d13815e779fcf4ace5
Daudi Amani <dsaronin@gmail.com>
2012-01-03 17:36:22 -0800

    0.1.5

--- lib/magesa.rb
+++ lib/magesa.rb
@@ -1,5 +1,5 @@
 # set up an environment variable to tell app where to find wkhtmlpdf binary
-version = File.exist?('VERSION') ? File.read('VERSION') : "0.1.3"
+version = File.exist?('VERSION') ? File.read('VERSION') : "0.1.5"
 
 gem_path = ( ENV['GEM_PATH'].blank?  ?  
             ENV['PATH'].split(':').first.sub(/\/bin/,"")   :  
